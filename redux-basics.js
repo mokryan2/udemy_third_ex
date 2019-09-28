3761a44ecbc064f2c1bc0ea63f4ce0501490f5e3
Ryan Mok <mokryan17@gmail.com>
2019-09-28 00:26:51 -0500

    Added in information on how to use dispatching action

--- redux-basics.js
+++ redux-basics.js
@@ -10,20 +10,18 @@ const rootReducer = (state = initialState, action) => {
     return state;
 };
 
-
 // ================================================= Store ===========================================
 const store = createStore(rootReducer);
-// In order for the store to work, there needs to first be a reducer!
+// In order for the store to work, there needs to first be a reducer! Always set reducer first!
 
 console.log(store.getState());
 
 
-
-
-
-
 // ========================================== Dispatching Action =====================================
-
+store.dispatch({ type: "INC_COUNTER"});
+// dispatch takes an argument that is an action that should be a JS object w/ a "type" property
+// Type is important b/c it helps get the info of what type of action was dispatched and what should happen in the reducer
+// Usual convention for the type is a string in ALL CAPS
 
 
 
