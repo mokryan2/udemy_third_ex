f5352ea05dfadd7f4d866fbdd0fa73fa350c84d2
Ryan Mok <mokryan17@gmail.com>
2019-09-28 01:13:12 -0500

    State stored in Redux is now being displayed in the DOM; however, can not change state via button clicks

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from "react-redux";
+// connect is a function that is used on a component to return a HOC
 
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
@@ -8,34 +10,41 @@ class Counter extends Component {
         counter: 0
     }
 
-    counterChangedHandler = ( action, value ) => {
-        switch ( action ) {
+    counterChangedHandler = (action, value) => {
+        switch (action) {
             case 'inc':
-                this.setState( ( prevState ) => { return { counter: prevState.counter + 1 } } )
+                this.setState((prevState) => { return { counter: prevState.counter + 1 } })
                 break;
             case 'dec':
-                this.setState( ( prevState ) => { return { counter: prevState.counter - 1 } } )
+                this.setState((prevState) => { return { counter: prevState.counter - 1 } })
                 break;
             case 'add':
-                this.setState( ( prevState ) => { return { counter: prevState.counter + value } } )
+                this.setState((prevState) => { return { counter: prevState.counter + value } })
                 break;
             case 'sub':
-                this.setState( ( prevState ) => { return { counter: prevState.counter - value } } )
+                this.setState((prevState) => { return { counter: prevState.counter - value } })
                 break;
         }
     }
 
-    render () {
+    render() {
         return (
             <div>
-                <CounterOutput value={this.state.counter} />
-                <CounterControl label="Increment" clicked={() => this.counterChangedHandler( 'inc' )} />
-                <CounterControl label="Decrement" clicked={() => this.counterChangedHandler( 'dec' )}  />
-                <CounterControl label="Add 5" clicked={() => this.counterChangedHandler( 'add', 5 )}  />
-                <CounterControl label="Subtract 5" clicked={() => this.counterChangedHandler( 'sub', 5 )}  />
+                <CounterOutput value={this.props.ctr} />
+                <CounterControl label="Increment" clicked={() => this.counterChangedHandler('inc')} />
+                <CounterControl label="Decrement" clicked={() => this.counterChangedHandler('dec')} />
+                <CounterControl label="Add 5" clicked={() => this.counterChangedHandler('add', 5)} />
+                <CounterControl label="Subtract 5" clicked={() => this.counterChangedHandler('sub', 5)} />
             </div>
         );
     }
 }
 
-export default Counter;
+const mapStateToProps = state => {
+    return {
+        ctr: state.counter
+    }
+};
+// This will store a function with expects state stored in redux as the input and returns a JS object which is a map of prop names and slices of the stored state
+
+export default connect(mapStateToProps)(Counter);
--- src/index.js
+++ src/index.js
@@ -2,11 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from "redux";
 import reducer from "./store/reducer";
+import { Provider } from "react-redux";
+// This package and helper component will allow us to hook up React w/ Redux by injecting the store into the React components
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 const store = createStore(reducer);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+
+    , document.getElementById('root'));
 registerServiceWorker();
