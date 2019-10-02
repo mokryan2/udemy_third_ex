f07e3f90ba19cdb773a6285a0de0ac5897c809ab
Ryan Mok <mokryan17@gmail.com>
2019-10-02 00:04:55 -0500

    Changed index.js to allow the combining of the reducer files

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from "redux";
-import reducer from "./store/reducer";
+import { createStore, combineReducers } from "redux";
+import counterReducer from "./store/reducers/counter";
+import resultsReducer from "./store/reducers/results";
 import { Provider } from "react-redux";
 // This package and helper component will allow us to hook up React w/ Redux by injecting the store into the React components
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore(reducer);
+const rootReducer = combineReducers({
+    ctr: counterReducer,
+    res: resultsReducer
+});
+// We need to have the index file combine the reducers first before creating the store;
+// thankfully, redux has the helper function that will combine the objects for us
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(
     <Provider store={store}>
