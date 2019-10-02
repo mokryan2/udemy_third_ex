f906a1378708f2f9e54067787c5684dce29b462a
Ryan Mok <mokryan17@gmail.com>
2019-10-01 23:38:20 -0500

    Created actions.js to store variables for actions

---
+++ src/store/actions.js
@@ -0,0 +1,8 @@
+export const INCREMENT = "INCREMENT";
+export const DECREMENT = "DECREMENT";
+export const ADD = "ADD";
+export const SUB = "SUB";
+export const MULTIPLY = "MULTIPLY";
+export const DIVIDE = "DIVIDE ";
+export const SAVE_RESULT = "SAVE_RESULT";
+export const REMOVE_RESULT = "REMOVE_RESULT";
