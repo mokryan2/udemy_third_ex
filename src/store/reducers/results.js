3c78209445ff0c845ead30f341041e964cac3b83
Ryan Mok <mokryan17@gmail.com>
2019-10-09 21:47:38 -0500

    Applied utility function to results.js

--- src/store/reducers/results.js
+++ src/store/reducers/results.js
@@ -1,4 +1,5 @@
 import * as actionTypes from "../actions/actionTypes";
+import { updateObject } from "../utility";
 
 const initialState = {
     results: []
@@ -7,18 +8,13 @@ const initialState = {
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.SAVE_RESULT:
-            return {
-                ...state,
+            return updateObject(state, {
                 results: state.results.concat({ id: new Date(), value: action.result })
-                // We need to change the state.counter value to action.result because the file no longer has local access to the state of the counter
-            }
+            });
+        // We need to change the state.counter value to action.result because the file no longer has local access to the state of the counter
         case actionTypes.REMOVE_RESULT:
             const newArray = state.results.filter(result => result.id !== action.resultElId);
-            return {
-                ...state,
-                results: newArray
-            }
-        // .filter is creating a new array to remain immutable state; result will return true if it doens't equal the clicked element
+            return updateObject(state, { results: newArray })
     }
     // Unlike how we typically use a switch statement, we don't need to include breaks after each case due to the fact return naturally exits out of the function
     return state
